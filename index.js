b61b25c644600289bb2f42076816f008e2f87d38
WlmWu <willwu0916@gmail.com>
2023-04-06 16:12:08 -0500

    pref: put functions into classes and improve function of storing data

--- index.js
+++ index.js
@@ -2,81 +2,160 @@ const LINE_NOTIFY_TOKEN = "<TOKEN_HERE>";
 // Developer Tool > Network > do search > do filter "home/search/rsList"
 const SEARCH_QUERY = ["https://rent.591.com.tw/home/search/rsList?is_format_data=1&is_new_list=1&type=1&region=4&kind=2&section=371,372,370&searchtype=1&rentprice=,10000&order=posttime&orderType=desc", "https://rent.591.com.tw/home/search/rsList?is_format_data=1&is_new_list=1&type=1&region=4&kind=3&section=371,372,370&searchtype=1&rentprice=,10000&order=posttime&orderType=desc"];
 
-var ss = SpreadsheetApp.getActiveSpreadsheet();
-var sheet = ss.getSheets()[0];
-const MAX_ROW_OF_SHEET = 100;
+const MAX_ROW_OF_SHEET = 120;
 
-function getCsrfToken() {
-    const rent591_url = "https://rent.591.com.tw/"
-    let response = UrlFetchApp.fetch(rent591_url);
-    let regex = new RegExp("<meta name=\"csrf-token\" content=\"([A-Za-z0-9]*)\">", "gi");
-    let csrfToken = regex.exec(response)[1];
-    const setCookie = response.getAllHeaders()["Set-Cookie"];
-    let cookie;
-    for (let i = 0; i < setCookie.length; i++) {
-        if (setCookie[i].includes("591_new_session")) {
-            cookie = setCookie[i];
-            break;
+class Crawler {
+    constructor() {
+        this.csrfToken = null;
+        this.cookie = null;
+        this.getCsrfToken();
+    }
+    get SEARCH_QUERY() {
+        return SEARCH_QUERY;
+    }
+    get HOST_URL() {
+        return "https://rent.591.com.tw/";
+    }
+
+    getCsrfToken() {
+        let response = UrlFetchApp.fetch(this.HOST_URL);
+        let regex = new RegExp("<meta name=\"csrf-token\" content=\"([A-Za-z0-9]*)\">", "gi");
+        let csrfToken = regex.exec(response)[1];
+        const setCookie = response.getAllHeaders()["Set-Cookie"];
+        let cookie;
+        for (let i = 0; i < setCookie.length; i++) {
+            if (setCookie[i].includes("591_new_session")) {
+                cookie = setCookie[i];
+                break;
+            }
+        }
+        [this.csrfToken, this.cookie] = [csrfToken, cookie];
+    }
+
+    getRegionNum(query) {
+        let regex = new RegExp(".*region=([0-9]*).*", "gi");
+        let regionNumber = regex.exec(query)[1];
+        return regionNumber;
+    }
+
+    crawling(searchQueryUrl) {
+        let regionNumber = this.getRegionNum(searchQueryUrl);
+
+        const header = {
+            "X-CSRF-TOKEN": this.csrfToken,
+            "Cookie": `${this.cookie}; urlJumpIp=${regionNumber};`,
+            'Content-Type': 'application/json'
+        }
+
+        const options = {
+            "method": "get",
+            "headers": header,
+            "muteHttpExceptions": true
+        };
+
+        const response = UrlFetchApp.fetch(searchQueryUrl, options);
+        return response.getContentText();
+    }
+
+    getCrawlingResult() {
+        let rtn = []
+        for (let i = 0; i < this.SEARCH_QUERY.length; i++) {
+            let rlt = JSON.parse(this.crawling(this.SEARCH_QUERY[i]))["data"]["data"].map(({ title, post_id, price, location }) => ({ title, post_id, price: price.replace(',', ''), location, url: `https://rent.591.com.tw/rent-detail-${post_id}.html` }));
+            rtn = [...rtn, ...rlt];
+        }
+        return rtn;
+    }
+}
+
+class Sheet {
+    constructor() {
+        this.ss = SpreadsheetApp.getActiveSpreadsheet();
+        this.sheet = this.ss.getSheets()[0];
+    }
+
+    get MAX_ROW_OF_SHEET() {
+        return MAX_ROW_OF_SHEET;
+    }
+
+    get length() {
+        // start from first row
+        return this.sheet.getDataRange().getNumRows();
+    }
+    getSheet() {
+        // This represents ALL the data  
+        let range = this.sheet.getDataRange();
+        let values = range.getValues();
+
+        let houses = []
+        for (var i = 1; i < values.length; i++) {
+            // console.log(values[i])
+            const listKey = ['title', 'post_id', 'price', 'location', 'url'];
+            const listValue = values[i];
+            const house = listKey.reduce((obj, key, index) => ({...obj, [key]: listValue[index] }), {});
+            houses.push(house);
+        }
+        return houses;
+    }
+    push(newRow) {
+        this.sheet.appendRow(Object.values(newRow));
+    }
+    pop() {
+        const START_ROW = 2;
+        this.sheet.deleteRow(START_ROW);
+    }
+    autoResize() {
+        if (this.length <= this.MAX_ROW_OF_SHEET) return;
+        const START_ROW = 2;
+        this.sheet.deleteRows(START_ROW, this.length - this.MAX_ROW_OF_SHEET);
+    }
+}
+
+class Notify {
+    get LINE_NOTIFY_URL() {
+        return "https://notify-api.line.me/api/notify";
+    }
+
+    get LINE_NOTIFY_TOKEN() {
+        return LINE_NOTIFY_TOKEN;
+    }
+
+    sendLineNotify(message) {
+        const header = {
+            "Authorization": `Bearer ${this.LINE_NOTIFY_TOKEN}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        const payload = {
+            "message": message,
+        }
+
+        const options = {
+            "method": "post",
+            "headers": header,
+            "payload": payload,
+            "muteHttpExceptions": true
+        };
+
+        UrlFetchApp.fetch(this.LINE_NOTIFY_URL, options);
+    }
+
+    itemObjToMessage(item) {
+        let itemMsg = JSON.parse(JSON.stringify(item));
+        itemMsg.price = "$ " + itemMsg.price;
+        delete itemMsg["post_id"];
+        return '\n' + Object.values(itemMsg).join('\n');
+    }
+
+    makeNotify(newRows) {
+        for (let i = 0; i < newRows.length; i++) {
+            this.sendLineNotify(this.itemObjToMessage(newRows[i]));
         }
     }
-
-    return [csrfToken, cookie]
 }
 
-function getRegionNum(query) {
-    let regex = new RegExp(".*region=([0-9]*).*", "gi");
-    let regionNumber = regex.exec(query)[1];
-
-    return regionNumber;
-}
-
-function crawling(searchQueryUrl) {
-    const [csrfToken, cookie] = getCsrfToken();
-    let regionNumber = getRegionNum(searchQueryUrl);
-
-    const header = {
-        "X-CSRF-TOKEN": csrfToken,
-        "Cookie": `${cookie}; urlJumpIp=${regionNumber};`,
-        'Content-Type': 'application/json'
-    }
-
-    const options = {
-        "method": "get",
-        "headers": header,
-        "muteHttpExceptions": true
-    };
-
-    const response = UrlFetchApp.fetch(searchQueryUrl, options);
-    return response.getContentText();
-}
-
-function getCrawlingResult() {
-    let rtn = []
-    for (let i = 0; i < SEARCH_QUERY.length; i++) {
-        let rlt = JSON.parse(crawling(SEARCH_QUERY[i]))["data"]["data"].map(({ title, post_id, price, location }) => ({ title, post_id, price: price.replace(',', ''), location, url: `https://rent.591.com.tw/rent-detail-${post_id}.html` }));
-        rtn = [...rtn, ...rlt];
-    }
-    return rtn;
-}
-
-function getCurrentItems() {
-    // This represents ALL the data
-    let range = sheet.getDataRange();
-    let values = range.getValues();
-
-    let houses = []
-    for (var i = 1; i < values.length; i++) {
-        // console.log(values[i])
-        const listKey = ['title', 'post_id', 'price', 'location', 'url'];
-        const listValue = values[i];
-        const house = listKey.reduce((obj, key, index) => ({...obj, [key]: listValue[index] }), {});
-        houses.push(house);
-    }
-    return houses;
-}
-
-function compareNewItem(dataCurrent, dataCrawling) {
-    let newItems = []
+function compareNewItem(dataCrawling, sheet) {
+    let dataCurrent = sheet.getSheet();
+    let newItems = [];
     for (let i = 0; i < dataCrawling.length; i++) {
         let dcrawling = dataCrawling[i];
         let duplicated = false;
@@ -96,66 +175,21 @@ function compareNewItem(dataCurrent, dataCrawling) {
     return newItems;
 }
 
-function storeNewItems(dataNew, dataCurrent) {
-    let itemsStored = [...dataNew, ...dataCurrent];
-    itemsStored = itemsStored.slice(0, MAX_ROW_OF_SHEET);
-
-    // clean all data
-    const START_ROW = 2;
-    const N_ROWS = sheet.getDataRange().getValues().length - 1;
-    if (N_ROWS > START_ROW)
-        sheet.deleteRows(START_ROW, N_ROWS);
-
-    // append the lastest nMax_ROW data
-    for (let i = 0; i < itemsStored.length; i++) {
-        let rlt = itemsStored[i];
-        Logger.log(`Stored: ${rlt.post_id}`);
-        sheet.appendRow(Object.values(rlt));
+function storeNewItems(dataNew, sheet) {
+    while (dataNew.length > 0) {
+        sheet.push(dataNew[0]);
+        dataNew.shift();
     }
+    sheet.autoResize();
 }
-
-function itemObjToMessage(item) {
-    let itemMsg = JSON.parse(JSON.stringify(item));
-    itemMsg.price = "$ " + itemMsg.price;
-    delete itemMsg["post_id"];
-    return '\n' + Object.values(itemMsg).join('\n');
-}
-
-function sendLineNotify(message) {
-    const lineNotifyUrl = "https://notify-api.line.me/api/notify";
-
-    const header = {
-        "Authorization": `Bearer ${LINE_NOTIFY_TOKEN}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    const payload = {
-        "message": message,
-    }
-
-    const options = {
-        "method": "post",
-        "headers": header,
-        "payload": payload,
-        "muteHttpExceptions": true
-    };
-
-    UrlFetchApp.fetch(lineNotifyUrl, options);
-}
-
-function makeNotify(dataNew) {
-    for (let i = 0; i < dataNew.length; i++) {
-        sendLineNotify(itemObjToMessage(dataNew[i]));
-    }
-}
-
 
 function main() {
-    // let tmp = {'title': '清大交大科學園區', 'post_id': 12731333, 'price': '5,200', 'location': '東區-金山六街', 'url': 'https://rent.591.com.tw/rent-detail-12731333.html'}
-
-    let dataCrawling = getCrawlingResult();
-    let dataCurrent = getCurrentItems();
-    let dataNew = compareNewItem(dataCurrent, dataCrawling);
-    makeNotify(dataNew);
-    storeNewItems(dataNew, dataCurrent);
+    // let testData = {'title': '清大交大科學園區', 'post_id': 12731333, 'price': '5,200', 'location': '東區-金山六街', 'url': 'https://rent.591.com.tw/rent-detail-12731333.html'};
+    const sheet = new Sheet();
+    const crawler = new Crawler();
+    const notifier = new Notify();
+    let dataCrawling = crawler.getCrawlingResult().reverse();
+    let dataNew = compareNewItem(dataCrawling, sheet);
+    notifier.makeNotify(dataNew);
+    storeNewItems(dataNew, sheet);
 }
